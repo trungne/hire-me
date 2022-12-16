c4b9705633ab893f4889232c31f4ff52099f41ac
Trung Nguyen <trung.nguyen@codelink.io>
2022-12-16 15:30:47 +0700

    chore: add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "shared/fb";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
