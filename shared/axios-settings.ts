853d4b0a1a2dec718adc9af1b1f1f746e93c032e
Trung Nguyen <trung.nguyen@codelink.io>
2023-01-10 01:56:53 +0700

    chore: deploy BE

--- shared/axios-settings.ts
+++ shared/axios-settings.ts
@@ -4,5 +4,8 @@ import { API_BASE_URL } from "./constants";
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
+  headers: {
+    "ngrok-skip-browser-warning": true,
+  },
 });
 export default axiosInstance;
